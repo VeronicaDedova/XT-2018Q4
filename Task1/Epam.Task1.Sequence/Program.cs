2057967377d86a4fad89d800651fd93053f8f6d0
Veronika_Dedova <veronikadedova@gmail.com>
2018-11-18 11:44:46 +0400

    Added Task1

---
+++ Task1/Epam.Task1.Sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Epam.Task1.Sequence
+{
+    class Program
+    {
+        static void Sequence(int N)
+        {
+            String seq = "";
+            for (int i = 1; i < N+1; i++)
+            {
+                seq += i+" ";
+            }
+            Console.WriteLine(seq);
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Enter the length of sequence:");
+            String input = Console.ReadLine();
+            if (Int32.TryParse(input, out n))
+            {
+                Sequence(n);
+            }
+            else
+                Console.WriteLine("Incorrect input");
+        }
+    }
+}
---
+++ Task1/Epam.Task1.Square/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Epam.Task1.Square
+{
+    class Program
+    {
+        static void Square(int N)
+        {
+            if (N%2 != 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if ((i == N/2) && (j == N/2))
+                            Console.Write(" ");
+                        else
+                            Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("Incorrect input");
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Enter the dimension of the square (an odd number):");
+            String input = Console.ReadLine();
+            if (Int32.TryParse(input, out n))
+            {
+                Square(n);
+            }
+            else
+                Console.WriteLine("Incorrect input");
+        }
+    }
+}
