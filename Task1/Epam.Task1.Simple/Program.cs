c1dab3b50e2b1927d868af32a2f8a98d63f63178
VeronicaDedova <45064749+VeronicaDedova@users.noreply.github.com>
2018-11-19 22:24:44 +0400

    Update Program.cs

--- Task1/Epam.Task1.Simple/Program.cs
+++ Task1/Epam.Task1.Simple/Program.cs
@@ -4,38 +4,54 @@ namespace Epam.Task1.Simple
 {
     class Program
     {
-        static void Simple(int N)
+        static void Simple(int n)
         {
             int count = 0;
-            if ((N == 0) || (N == 1))
+            
+            if ((n == 0) || (n == 1))
+            {
                 Console.WriteLine("The number is NOT simple");
+            }
+            
             else
             {
-                for (int i = 2; i <= Math.Sqrt(N); i++)
+                for (int i = 2; i <= Math.Sqrt(n); i++)
                 {
-                    if (N % i == 0)
+                    if (n % i == 0)
                     {
                         count++;
                         break;
-                    }
+                    }                    
                 }
+                
                 if (count != 0)
+                {
                     Console.WriteLine("The number is NOT simple");
+                }
+                
                 else
+                {
                     Console.WriteLine("The number is simple");
+                }
+                
             }                
         }
         static void Main(string[] args)
         {
             int n = 0;
+            
             Console.WriteLine("Enter the number:");
             String input = Console.ReadLine();
+            
             if (Int32.TryParse(input, out n))
             {
                 Simple(n);
             }
+            
             else
+            {
                 Console.WriteLine("Incorrect input");
+            }
         }
     }
 }
