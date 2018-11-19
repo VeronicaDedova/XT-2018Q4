2ccc7ec0d5b011cac78b764e13a38f639c9c108a
VeronicaDedova <45064749+VeronicaDedova@users.noreply.github.com>
2018-11-19 22:22:23 +0400

    Update Program.cs

--- Task1/Epam.Task1.Sequence/Program.cs
+++ Task1/Epam.Task1.Sequence/Program.cs
@@ -4,26 +4,31 @@ namespace Epam.Task1.Sequence
 {
     class Program
     {
-        static void Sequence(int N)
+        static void Sequence(int n)
         {
-            String seq = "";
-            for (int i = 1; i < N+1; i++)
+            for (int i = 1; i < n+1; i++)
             {
-                seq += i+" ";
+                Console.Write(i + " ");
             }
-            Console.WriteLine(seq);
+            
         }
         static void Main(string[] args)
         {
             int n = 0;
+            
             Console.WriteLine("Enter the length of sequence:");
             String input = Console.ReadLine();
+            
             if (Int32.TryParse(input, out n))
             {
                 Sequence(n);
             }
+            
             else
+            {
                 Console.WriteLine("Incorrect input");
+            }
+            
         }
     }
 }
