528845b7ed498f4d3df01cc08d4220b133d9d453
VeronicaDedova <45064749+VeronicaDedova@users.noreply.github.com>
2018-11-19 22:27:16 +0400

    Update Program.cs

--- Task1/Epam.Task1.Square/Program.cs
+++ Task1/Epam.Task1.Square/Program.cs
@@ -4,36 +4,54 @@ namespace Epam.Task1.Square
 {
     class Program
     {
-        static void Square(int N)
+        static void Square(int n)
         {
-            if (N%2 != 0)
+            if (n%2 != 0)
             {
-                for (int i = 0; i < N; i++)
+                for (int i = 0; i < n; i++)
                 {
-                    for (int j = 0; j < N; j++)
+                    for (int j = 0; j < n; j++)
                     {
-                        if ((i == N/2) && (j == N/2))
+                        if ((i == n/2) && (j == n/2))
+                        {
                             Console.Write(" ");
+                        }
+                        
                         else
+                        {
                             Console.Write("*");
+                        }
+                        
                     }
+                    
                     Console.WriteLine();
                 }
+                
             }
+            
             else
+            {
                 Console.WriteLine("Incorrect input");
+            }
+            
         }
+        
         static void Main(string[] args)
         {
             int n = 0;
+            
             Console.WriteLine("Enter the dimension of the square (an odd number):");
             String input = Console.ReadLine();
+            
             if (Int32.TryParse(input, out n))
             {
                 Square(n);
             }
+            
             else
+            {
                 Console.WriteLine("Incorrect input");
+            }
         }
     }
 }
