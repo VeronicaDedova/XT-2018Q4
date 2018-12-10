6d4fdf30795084c76ebd95efc254da387c9a8ddf
Veronika_Dedova <veronikadedova@gmail.com>
2018-12-10 19:56:31 +0400

    Added task4 (lost, wordfrequency)

---
+++ Task4/Epam.Task4.Lost/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task4.Lost
+{
+    public class Program
+    {
+        public static LinkedList<int> Create(int n)
+        {
+            var list = new LinkedList<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                list.AddLast(i);
+            }
+
+            return list;
+        }
+
+        public static void Show(LinkedList<int> list)
+        {
+            Console.Write("Stayed: ");
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Remove(LinkedList<int> list)
+        {
+            var current = list.First;
+
+            while (list.Count != 1)
+            {
+                if (current.Next != null)
+                {
+                    Console.WriteLine($"{current.Next.Value} removed");
+                }
+                else
+                {
+                    Console.WriteLine($"{list.First.Value} removed");
+                }
+
+                list.Remove(current.Next ?? list.First);
+                current = current.Next ?? list.First;         
+                Show(list);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"There are N people in the circle, numbered from 1 to N.{Environment.NewLine}When maintaining accounts in a circle, every second person is crossed out until one is left.");
+
+            do
+            {
+                Console.Write($"{Environment.NewLine}Enter the number of people: ");
+                string sn = Console.ReadLine();
+
+                if (int.TryParse(sn, out int n))
+                {
+                    LinkedList<int> list = Create(n);
+                    Remove(list);
+                    Console.WriteLine("Try again? (y/n) ");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input.");
+                    Console.WriteLine("Try again? (y/n) ");
+                }
+            }
+            while (Console.ReadKey().Key == ConsoleKey.Y);
+        }
+    }
+}
---
+++ Task4/Epam.Task4.WordFrequency/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task4.WordFrequency
+{
+    public class Program
+    {
+        public static List<string> Create(string[] word)
+        {
+            var wordList = new List<string>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordList.Add(word[i]);
+            }
+
+            return wordList;
+        }
+
+        public static void Show(List<string> wordList)
+        {
+            Console.WriteLine($"{Environment.NewLine}Single words: ");
+
+            foreach (var item in wordList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Frequency(List<string> wordList)
+        {
+            int wordCounter = 0;
+
+            Console.WriteLine($"{Environment.NewLine}Word frequency: ");
+
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                for (int j = i; j < wordList.Count; j++)
+                {
+                    if (wordList[i].Equals(wordList[j]))
+                    {
+                        wordCounter++;
+                    }
+
+                    if (wordCounter > 1 && wordList[i].Equals(wordList[j]))
+                    {
+                        wordList.RemoveAt(j);
+                        j--;
+                    }
+                }
+
+                Console.WriteLine($"{wordList[i]}: {wordCounter}");
+                wordCounter = 0;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Enter the english text: ");
+                string text = Console.ReadLine().ToLower();
+
+                char[] separator = new char[] { ' ', '.' };
+                string[] word = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+                List<string> wordList = Create(word);
+                Show(wordList);
+                Frequency(wordList);
+                Console.WriteLine("Try again? (y/n) ");
+            }
+            while (Console.ReadKey().Key == ConsoleKey.Y);    
+        }
+    }
+}
