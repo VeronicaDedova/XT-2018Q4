6e114429cbf756d359ce4d256f8e9f22190a977c
Veronika_Dedova <veronikadedova@gmail.com>
2019-01-31 23:24:11 +0400

    added task10

---
+++ Task10/Epam.Task10.SimpleGallery/script.js
@@ -0,0 +1,54 @@
+"use strict"
+
+var nextPage = document.getElementById("timer").getAttribute("href");
+var timeout;
+
+function startTimer(){
+    var timer = document.getElementById("timer");
+    var time = timer.innerHTML;
+    var arr = time.split(":");
+    var minutes = arr[0];
+    var seconds = arr[1];
+   
+    if (seconds == 0){
+        if (nextPage == ""){
+            var repeat = confirm('Повторить?');
+            if(repeat){
+                window.location.href = "SimpleGallery1.html";
+            }
+            else{
+                alert('Пока:)');
+            }
+        }
+        else{
+            window.location.href = nextPage;
+            return;
+        }            
+    }    
+    seconds--;
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }   
+    
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+    timeout = setTimeout(startTimer, 1000);
+}
+
+function toPreviousPage(){
+    var previousPage = document.getElementById("previous").getAttribute("href");
+    if(previousPage == ""){
+        alert('Это первая страница.');
+    }
+    else{
+        window.location.href = previousPage;
+    }  
+}
+
+function pause(){
+    clearTimeout(timeout);
+}
+
+function play(){
+    setTimeout(startTimer, 1000);
+}
